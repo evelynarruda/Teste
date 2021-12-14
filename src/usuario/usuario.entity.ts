c1d4e521b1b511682f01ca1ed115b72bb7f78384
Evelyn <evelynarrudasilva@gmail.com>
2021-12-13 23:29:29 -0300

    Commit inicial

---
+++ src/usuario/usuario.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Usuario {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 500 })
+  nome: string;
+
+  @Column({ length: 100 })
+  email: string;
+
+  @Column({ length: 255 })
+  senha: string;
+
+  @Column({length: 15})
+  telefone: string;
+
+  @Column({length: 14})
+  cpf: string;
+  
+  @Column()
+  postagem: string;
+}
